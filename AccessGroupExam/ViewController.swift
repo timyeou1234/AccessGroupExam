f603f13711a90d8599dbbf212552150f69570b71
Timothy Yeou <timyeou1234@hotmail.com>
2024-04-30 10:39:13 +0800

    Initial Commit

---
+++ AccessGroupExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccessGroupExam
+//
+//  Created by Tim on 2024/4/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
